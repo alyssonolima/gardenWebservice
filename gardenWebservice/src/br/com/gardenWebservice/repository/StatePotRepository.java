208c5cf16c04566243eecba9d94fb6bba8571996
Alysson Lima <Alysson Lima@10.7.228.78>
2015-10-20 22:27:41 -0300

    ajustes Statepot

--- gardenWebservice/src/br/com/gardenWebservice/repository/StatePotRepository.java
+++ gardenWebservice/src/br/com/gardenWebservice/repository/StatePotRepository.java
@@ -3,10 +3,12 @@ package br.com.gardenWebservice.repository;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import br.com.gardenWebservice.dao.Fabrica;
+import br.com.gardenWebservice.entity.Pot;
 import br.com.gardenWebservice.entity.StatePot;
 
 public class StatePotRepository {
@@ -14,16 +16,53 @@ public class StatePotRepository {
 	
 	private JdbcTemplate jt;
 	
-	public List<StatePot> findStatePot(){
+	public List<StatePot> findStatePot(int idPot) throws SQLException{
+		StringBuilder sql = new StringBuilder(
+			" SELECT * " +
+			" FROM connected_garden.state_pot " +
+			" WHERE ID_POT = " + idPot
+		);
 		
+		jt = new JdbcTemplate(Fabrica.getDataSource());	
+		List<Map<String, Object>> result = jt.queryForList(sql.toString());
 		List<StatePot> states = new ArrayList<StatePot>();
+		
+		for(Map<String, Object> map: result){
+			StatePot state = new StatePot();
+			state.setId((int)map.get("ID_STATE_POT"));
+			state.setIdPot((int)map.get("ID_POT"));
+			state.setDescription((String)map.get("DESCRIPTION"));
+			state.setTemp((int) map.get("TEMP"));
+			state.setHumAir((int) map.get("HUM_AIR"));
+			state.setHumSoil((int) map.get("HUM_SOIL"));
+			state.setLight((int) map.get("LIGHT"));
+			Boolean active = (Boolean) map.get("ACTIVE");
+			state.setActive(active);
+			Boolean auto = (Boolean) map.get("IRRIGATION_AUTO");
+			state.setIrrigationAuto(auto);
+			Boolean irriActive = (Boolean) map.get("IRRIGATION_ACTIVE");
+			state.setIrrigationActive(irriActive);
+			
+			states.add(state);
+		}
+		
 		return states;
 	}
 	
-	public void insertStatePot() throws SQLException{
+	public void insertStatePot(StatePot state) throws SQLException{
 		StringBuilder sql = new StringBuilder(
-				
-				
+			" INSERT INTO `connected_garden`.`state_pot` " +
+			" (`ID_POT`, `DESCRIPTION`, `TEMP`, `HUM_AIR`, `HUM_SOIL`, `LIGHT`, `ACTIVE`, `IRRIGATION_AUTO`, `IRRIGATION_ACTIVE`) " +
+			" VALUES ('" + 
+			state.getIdPot() + "', '" + 
+			state.getDescription() + "', '" + 
+			state.getTemp() + "', '" + 
+			state.getHumAir() + "', '" + 
+			state.getHumSoil() + "', '" + 
+			state.getLight() + "', " + 
+			state.isActive() + ", " + 
+			state.isIrrigationAuto() + ", " + 
+			state.isIrrigationActive() + ")"				
 		);
 		
 		jt = new JdbcTemplate(Fabrica.getDataSource());
@@ -31,10 +70,19 @@ public class StatePotRepository {
 		jt.getDataSource().getConnection().close();		
 	}
 	
-	public void updateStatePot(int id) throws SQLException{
+	public void updateStatePot(StatePot state) throws SQLException{
 		StringBuilder sql = new StringBuilder(
-				
-		
+			" UPDATE `connected_garden`.`state_pot` " +
+			" SET " +
+			"`ID_POT`=" + state.getIdPot() + ", " +
+			"`DESCRIPTION`='" + state.getDescription() + "', " +
+			"`TEMP`='" + state.getTemp() + "', " +
+			"`HUM_AIR`='" + state.getHumAir() + "', " +
+			"`HUM_SOIL`='" + state.getHumSoil() + "', " +
+			"`ACTIVE`= " + state.isActive() + ", " +
+			"`IRRIGATION_AUTO`= " + state.isIrrigationAuto() + ", " +
+			"`IRRIGATION_ACTIVE`= " + state.isIrrigationActive() + " " +
+			" WHERE `ID_STATE_POT`= " + state.getId()		
 		);
 		
 		jt = new JdbcTemplate(Fabrica.getDataSource());
@@ -44,7 +92,8 @@ public class StatePotRepository {
 	
 	public void deleteStatePot(int id) throws SQLException{
 		StringBuilder sql = new StringBuilder(
-				
+			" DELETE FROM `connected_garden`.`state_pot` " +
+			" WHERE `ID_STATE_POT`= " + id
 		);
 		
 		jt = new JdbcTemplate(Fabrica.getDataSource());
--- gardenWebservice/src/br/com/gardenWebservice/resources/StatePotResource.java
+++ gardenWebservice/src/br/com/gardenWebservice/resources/StatePotResource.java
@@ -1,5 +1,6 @@
 package br.com.gardenWebservice.resources;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -9,6 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import br.com.gardenWebservice.entity.StatePot;
 import br.com.gardenWebservice.repository.StatePotRepository;
@@ -21,29 +23,34 @@ public class StatePotResource {
 	@GET
 	@Path(value = "/one")
 	@Produces("application/json")
-	public List<StatePot> getStatePot(){
+	public List<StatePot> getStatePot(@QueryParam("idPot") String idPot) throws SQLException{
+		Integer id = Integer.parseInt(idPot);
 		
-		return dao.findStatePot();
+		return dao.findStatePot(id);
 	}
 	
 	@POST
 	@Path(value = "/one")
 	@Consumes("application/json")
-	public void postStatePot(){
+	public void postStatePot(StatePot state) throws SQLException{
 		
+		dao.insertStatePot(state);		
 	}
 	
 	@PUT
 	@Path(value = "/one")
 	@Consumes("application/json")
-	public void updateStatePot(){
+	public void updateStatePot(StatePot state) throws SQLException{
 		
+		dao.updateStatePot(state);		
 	}
 	
 	@DELETE
 	@Path(value = "/one")
 	@Consumes("application/json")
-	public void deleteStatePot(){
+	public void deleteStatePot(String idStatePot) throws SQLException{
+		Integer id = Integer.parseInt(idStatePot);
 		
+		dao.deleteStatePot(id);		
 	}
 }
